b0d75981b2d09998a4636f96a495a7e0c0a8fe6d
Enderson <gabrielgomez2040@gmail.com>
2023-05-26 17:55:22 -0400

    actividad individual2-version1

---
+++ app/src/main/java/com/example/estudios/cursoAndroid/RegistroUsuarioActivity.java
@@ -0,0 +1,16 @@
+package com.example.estudios.cursoAndroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.estudios.R;
+
+public class RegistroUsuarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_usuario);
+    }
+}
